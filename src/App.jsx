4f360d553d38f07fe9aea11d5b993c01ba764e05
Nikolai Hesterberg <nikolai.hesterberg@gmail.com>
2024-03-10 22:23:29 +0100

    implemented the recalc function

--- src/App.jsx
+++ src/App.jsx
@@ -113,70 +113,83 @@ const App = () => {
     }
   }
 
-  const recalc_elo = () => {
-    let allPlayers = []
-    let allMatches = []
+  const recalc_elo = async () => {
+    let allMatches = await matchService.getAll()
 
     eloService.deleteAll()
 
-    allPlayers.forEach((player) => {
+    for (const player of players) {
       
       const updatedPlayer = {
         name: player.name,
         elo: 1200
       }
 
-      playerService.update(player.id, updatedPlayer)
-        .then(newPlayer1Obj => {
-          setPlayers(players.map(p => p.id !== newPlayer1Obj.id ? p : newPlayer1Obj).sort(compareElo))
-      })
-    })
+      const newPlayerObj = await playerService.update(player.id, updatedPlayer)
+    }
 
-    playerService
-      .getAll()
-      .then((data) => {
-        allPlayers = data
-      })
+    const ps = await playerService.getAll()
+    setPlayers(ps)
 
-    matchService
-      .getAll()
-      .then((data) => {
-        allMatches = data
-      })
+    for (const match of allMatches) {
+      const allPlayers = await playerService.getAll()
 
-    allMatches.forEach((match) => {
-      let p1_index = allPlayers.findIndex((player) => player.n == match.p1)
-      let p2_index = allPlayers.findIndex((player) => player.n == match.p2)
-
-      let p1 = players[p1_index]
-      let p2 = players[p2_index]
+      let p1 = allPlayers.find(p => p.id === match.p1.id) 
+      let p2 = allPlayers.find(p => p.id === match.p2.id) 
 
       let updated_elo = calc_elo(p1.elo, p2.elo, match.s1, match.s2)
 
-      let p1_new_elo = updated_elo.p1_updated_elo
-      let p2_new_elo = updated_elo.p2_updated_elo
+      let newMatchObj = {
+        date: match.date,
+        p1: p1.id,
+        p2: p2.id,
+        s1: match.score,
+        s2: match.score,
+        elo1: updated_elo.p1_updated_elo,
+        elo2: updated_elo.p2_updated_elo,
+      }
 
-      console.log(p1_new_elo, p2_new_elo)
+      const retMatchObj = await matchService.update(match.id, newMatchObj)
 
-      p1.elo = p1_new_elo
-      p2.elo = p2_new_elo
-
-      players[p1_index] = p1
-      players[p2_index] = p2
-
-      match.elo1 = p1_new_elo
-      match.elo2 = p2_new_elo
-
-      setPlayers([...players])
-      setMatches([...matches])
-
+      const elo1Object = {
+        player: p1.id,
+        match: retMatchObj.id,
+        elo: updated_elo.p1_updated_elo
+      }
+  
+      const elo2Object = {
+        player: p2.id,
+        match: retMatchObj.id,
+        elo: updated_elo.p2_updated_elo
+      }
+  
+      const newElo1Obj = await eloService.create(elo1Object)
+  
+      const newElo2Obj = await eloService.create(elo2Object)
+  
+      const p1UpdateObject = {
+        name: p1.name,
+        elo: updated_elo.p1_updated_elo
+      }
+  
+      const p2UpdateObject = {
+        name: p2.name,
+        elo: updated_elo.p2_updated_elo
+      }
+  
+      const newPlayer1Obj = await playerService.update(p1.id, p1UpdateObject)
+      const newPlayer2Obj = await playerService.update(p2.id, p2UpdateObject)
+      
       console.log(players)
+    }
 
-      console.log(p1)
-      console.log(p2)
-    })
+    allMatches = await matchService.getAll()
+    const allPlayers = await playerService.getAll()
+    const allElo = await eloService.getAll()
 
-    console.log(players)
+    setMatches(allMatches.sort(compareDates).slice(0,10))
+    setPlayers(allPlayers.sort(elo))
+    setEloArray(allElo)
   }
 
   const addMatch = async (event) => {
--- src/components/Match.jsx
+++ src/components/Match.jsx
@@ -6,8 +6,8 @@ const Match = ({date,p1,p2,s1,s2,elo1,elo2}) => {
       <td>{p2}</td>
       <td>{s1}</td>
       <td>{s2}</td>
-      <td>{elo1.toFixed(1)}</td>
-      <td>{elo2.toFixed(1)}</td>
+      <td>{elo1}</td>
+      <td>{elo2}</td>
     </tr>
   )
 }
