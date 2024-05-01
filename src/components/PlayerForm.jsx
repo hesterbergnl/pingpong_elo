a46387c0e5b2dbb806a1fee24829a863d3e2c859
Nikolai Hesterberg <nikolai.hesterberg@gmail.com>
2024-05-01 21:47:35 +0200

    removed unnecessary import

--- src/components/PlayerForm.jsx
+++ src/components/PlayerForm.jsx
@@ -1,4 +1,4 @@
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { createPlayer } from '../reducers/playerReducer'
 import { createElos } from '../reducers/eloReducer'
 
