893bcace0accd9a34ca47ffe15b302583c8fca62
Nikolai Hesterberg <nikolai.hesterberg@gmail.com>
2024-05-05 22:08:24 +0200

    removed all references to elo

--- src/store.js
+++ src/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 
-import eloReducer from './reducers/eloReducer'
 import matchReducer from './reducers/matchReducer'
 import playerReducer from './reducers/playerReducer'
 import selectedPlayerReducer from './reducers/selectedPlayerReducer'
 
 const store = configureStore({
   reducer: {
-    elos: eloReducer,
     matches: matchReducer,
     players: playerReducer,
     selectedPlayer: selectedPlayerReducer,
